6dcdedc2a791440fd106208367d732e1cf8be42d
Tharun Kumar <tharunk1705@gmail.com>
2021-04-23 10:47:31 +0530

    added option to delete the created resource

--- controllers/resource.js
+++ controllers/resource.js
@@ -1,5 +1,17 @@
 const Resource = require('../models/resource');
 
+exports.getResourceById = (req, res, next, id) => {
+    Resource.findById(id).exec((err, resource) => {
+        if( err || !resource) {
+            return res.status(400).json({
+                error : "Resource Not Found!"
+            });
+        }
+        req.resource = resource;
+        next();
+    })
+}
+
 exports.createResource = (req, res) => {
     const resource = new Resource(req.body);
     resource.save((err, newResource) => {
@@ -38,4 +50,19 @@ exports.getMyResources = (req, res) => {
             res.json(resources);
         }
     );
+}
+
+exports.deleteResource = (req, res) => {
+    
+    const resource = req.resource;
+    resource.remove((err, resource) => {
+        if(err) {
+            return res.status(400).json({
+                error : "Failed to Remove Category!"
+            });
+        }
+        res.json({
+            message : `Successfully deleted ${resource.resourceName}`
+        });
+    })
 }
--- routes/resource.js
+++ routes/resource.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { getUserById} = require("../controllers/user");
-const { createResource, getAllResources, getMyResources} = require("../controllers/resource");
+const { getResourceById, createResource, getAllResources, getMyResources, deleteResource} = require("../controllers/resource");
 
 router.param("userId", getUserById);
-
+router.param("resourceId", getResourceById);
 
 router.post("/resource/create", createResource);
 router.get("/resource/available", getAllResources);
 router.get("/user/:userId/resource", getMyResources);
+
+router.delete("/user/:userId/resource/:resourceId", deleteResource)
 module.exports = router;
